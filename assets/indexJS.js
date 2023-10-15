347c36e0b66ed9b2d11759e1fd1e96c5aee75bc5
bstewlipsey <brandonlipsey11@gmail.com>
2023-10-15 18:04:53 -0400

    finishing up js

--- assets/indexJS.js
+++ assets/indexJS.js
@@ -1,23 +1,53 @@
 window.onload = function() {
     var trans1 = document.getElementById("img")
     var trans2 = document.getElementById("img2")
+    var carousel = document.getElementById("carousel")
 
-    trans1.setAttribute("style","display: block; bottom: 40%; ")
+    trans1.style.zIndex = 2
+    trans2.style.zIndex = 1
+
+    var carouselLength = 9
+    var carouselIndex = 1
+    var position = ["0%","0%", "0%", "0%","0","0%","0%", "0%", "0%"]
+
+    var image = 'assets/images/Carousel/img' +carouselIndex+'.jpg'
+    trans1.setAttribute("src",image)
+    trans1.style.top = position[carouselIndex-1]
+    trans1.style.display = "flex"
 
     var interval = setInterval(changeImg,5000)
 
-    img.addEventListener("mouseenter", () => {
+    carousel.addEventListener("mouseenter", () => {
         clearInterval(interval);
     });
       
-    img.addEventListener("mouseleave", () => {
+    carousel.addEventListener("mouseleave", () => {
         interval = setInterval(changeImg, 5000);
     });
 
-    img.addEventListener("mousedown", () => {
-        changeImg()
-    });
     function changeImg(){
-        console.log("hi")
+        var opacity = 1;
+
+        image = 'assets/images/Carousel/img' +carouselIndex+'.jpg'
+        trans2.setAttribute("src",image)
+        trans2.style.top = position[carouselIndex-1]
+        trans2.style.display = "flex"
+        
+        const fadeInterval = setInterval(function () {
+            if (opacity <= 0) {
+                image = 'assets/images/Carousel/img' +carouselIndex+'.jpg'
+                trans1.setAttribute("src",image)
+                trans1.style.top = position[carouselIndex-1]
+                console.log(carouselIndex)
+                carouselIndex++
+                clearInterval(fadeInterval)
+            } else {
+                opacity -= 0.01;
+                trans1.style.opacity = opacity;
+            }
+        }, 20);
+        if(carouselIndex >=carouselLength){
+            carouselIndex = 1
+        }
     }
 }
